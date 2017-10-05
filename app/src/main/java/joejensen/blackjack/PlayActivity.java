9751e1d9b1bdb4af10c69396b0188d32c3f4d52b
Joseph Jensen <josephpjensen@gmail.com>
2017-10-04 20:28:21 -0500

    Added player action buttons

--- app/src/main/java/joejensen/blackjack/PlayActivity.java
+++ app/src/main/java/joejensen/blackjack/PlayActivity.java
@@ -3,11 +3,20 @@ package joejensen.blackjack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Game game;
     private static Settings settings;
+
+    private Button hitButton;
+    private Button standButton;
+    private Button splitButton;
+    private Button doubleDownButton;
+    private Button changeBetButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,62 @@ public class PlayActivity extends AppCompatActivity {
         Toolbar playToolbar = (Toolbar) findViewById(R.id.play_toolbar);
         setSupportActionBar(playToolbar);
 
+        initButtons();
         game = new Game();
     }
 
     private void initButtons() {
+        hitButton = (Button) findViewById(R.id.hit_button);
+        standButton = (Button) findViewById(R.id.stand_button);
+        splitButton = (Button) findViewById(R.id.split_button);
+        doubleDownButton = (Button) findViewById(R.id.double_down_button);
+        changeBetButton = (Button) findViewById(R.id.change_bet_button);
+
+        hitButton.setOnClickListener(this);
+        standButton.setOnClickListener(this);
+        splitButton.setOnClickListener(this);
+        doubleDownButton.setOnClickListener(this);
+        changeBetButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.hit_button:
+                onHit();
+                break;
+            case R.id.stand_button:
+                onStand();
+                break;
+            case R.id.double_down_button:
+                onDoubleDown();
+                break;
+            case R.id.split_button:
+                onSplit();
+                break;
+            case R.id.change_bet_button:
+                onChangeBet();
+                break;
+        }
+    }
+
+    private void onHit() {
 
     }
 
+    private void onStand() {
+
+    }
+
+    private void onDoubleDown() {
+
+    }
+
+    private void onSplit() {
+
+    }
+
+    private void onChangeBet() {
+
+    }
 }
